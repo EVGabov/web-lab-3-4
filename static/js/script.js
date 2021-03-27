31ac1854670fc1a112e7c87e8f331c4dfc610c37
EVGabov <72019030+EVGabov@users.noreply.github.com>
2021-03-27 16:50:06 +0500

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,127 @@
+var flag=0
+const deleteTask = (id) => {
+    const url = '/tasks/' + id;
+
+    fetch(url, {
+        method: 'DELETE'
+    })
+    .then(() => {
+        const card = document.querySelector('.card#id_' + id);
+        card.remove();
+    })
+    .catch((e) => {
+        alert(e.message)
+    })
+}
+
+const setState = (id) => {
+    const url = '/tasks/' + id;
+
+    fetch(url, {
+        method: 'PUT'
+    })
+    .catch((e) => {
+        alert(e.message)
+    })
+}
+
+const getComment = (id) => {
+    if(flag==0){
+    const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            for (let i = 0; i < 5; i++) {
+                const comments = document.getElementById('this_card');
+                const comment = document.createElement('p');
+                comment.innerText = json[i]['body'];
+                comments.appendChild(comment);
+            }
+        })
+        flag++;
+        }
+}
+
+const changeTask = (id) => {
+    const url = '/tasks/desc/' + id;
+    const newDesc = document.getElementById("newDesc")
+    fetch(url, {
+        method: 'PUT',
+        body: newDesc.value
+    })
+}
+
+const filter = () => {
+    const filters = document.getElementById('filters')
+    const selectedState = filters.options[filters.selectedIndex].value
+    const filterTitle = document.getElementById('filterTitle')
+    const selectedTitle = filterTitle.value
+    const url = '/filters'
+    fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            state: selectedState,
+            title: selectedTitle
+        })
+    })
+    .then(response => response.json())
+    .then(json => renderTasks(json))
+}
+
+const renderTasks = (tasksData) => {
+    const tasks = document.getElementById("tasks")
+    tasks.innerHTML = ''
+    for (let i = 0; i < tasksData.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card', 'mb-4')
+        card.id = 'id_' + tasksData[i].id
+        card.style.width = '18rem'
+
+        const cardHeader = document.createElement('h4')
+        cardHeader.classList.add('card-header')
+
+        const cardState = document.createElement('input')
+        cardState.type = 'checkbox'
+        cardState.id = 'check_' + tasksData[i].id
+        cardState.setAttribute('onclick', 'setState(' + tasksData[i].id + ')')
+        if (tasksData[i].state === 1)
+            cardState.setAttribute('checked','checked')
+        cardState.style.marginRight = '3px'
+
+        cardHeader.appendChild(cardState)
+        cardHeader.innerHTML += tasksData[i].title
+        card.appendChild(cardHeader)
+
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+        const taskDesc = document.createElement('p')
+        taskDesc.innerText = tasksData[i].description
+
+        const openButton = document.createElement('a')
+        openButton.classList.add('btn', 'btn-primary')
+        openButton.innerText = 'Открыть'
+        openButton.setAttribute('href', '/tasks/' + tasksData[i].id)
+
+        const deleteButton = document.createElement('button')
+        deleteButton.innerText = 'Удалить'
+        deleteButton.classList.add('btn', 'btn-primary')
+        deleteButton.setAttribute('onclick', 'deleteTask(' + tasksData[i].id + ')')
+        deleteButton.style.marginLeft = '3px'
+
+        cardBody.append(taskDesc, openButton, deleteButton)
+        card.appendChild(cardBody)
+
+        tasks.appendChild(card)
+                    
+    //                 // <div class="card-body">
+    //                 //     <p>{{ task.description }}</p>
+    //                 // <a href="/tasks/{{task.id}}" class="btn btn-primary">Открыть</a>
+    //                 // <button class="btn btn-primary" onclick="deleteTask({{task.id}})">Удалить</a>
+    //                 // </div>
+    // }
+    }
+}
---
+++ storage/__init__.py
@@ -0,0 +1,123 @@
+import sqlite3
+from pathlib import Path
+from werkzeug.security import check_password_hash, generate_password_hash
+from entities import User, Task
+
+# Подключаемся к БД
+db_path = '/'.join([str(Path(__file__).parent), '..', 'db', 'database.sqlite'])
+db = sqlite3.connect(db_path, check_same_thread=False)
+
+
+class Storage:
+    @staticmethod
+    def add_user(user: User):
+        """Добавление пользователя
+        :param user:    новый пользователь
+        :type user:     User"""
+        db.execute('INSERT INTO users (email, password) VALUES (?, ?)',
+                   (user.email, generate_password_hash(user.password)))
+        db.commit()
+
+    @staticmethod
+    def get_user_by_email_and_password(email: str, passwordHash: str) -> User:
+        """Найти пользователя по email и паролю
+        :param email:       электронная почта
+        :type email:        str
+        :param passwordHash:    хэш пароля
+        :type passwordHash:     str
+        :return: пользователь
+        :rtype: User
+        """
+        user_data = db.execute('SELECT * FROM users WHERE email=?', (email,)).fetchone()
+        if user_data and check_password_hash(user_data[2], passwordHash):
+            return User(id=user_data[0], email=user_data[1], password=user_data[2])
+        else:
+            return None
+
+    @staticmethod
+    def get_user_by_id(id: int) -> User:
+        """Найти пользователя по id
+        :param id:  идентификатор пользователя
+        :type id:   int
+        :return:    пользователь
+        :rtype:     User"""
+        user_data = db.execute('SELECT * FROM users WHERE id=?', (id,)).fetchone()
+        if user_data:
+            return User(id=user_data[0], email=user_data[1], password=user_data[2])
+        else:
+            return None
+
+    @staticmethod
+    def get_tasks(user_id: int) -> list:
+        request = db.execute('SELECT * FROM tasks WHERE id_user = ?', (user_id,));
+        tasks = request.fetchall();
+        tasksList = [];
+        for task in tasks:
+            tasksList.append(Task(task[0], task[1], task[2], task[3], task[4]));
+        return tasksList;
+
+    @staticmethod
+    def add_task(task: Task):
+        db.execute('INSERT INTO tasks (title, description, id_user, is_done) VALUES (?, ?, ?, ?)',
+            (task.title, task.description, task.user_id, task.state));
+        db.commit();
+
+    @staticmethod
+    def delete_task(taskId: int):
+        db.execute('DELETE FROM tasks WHERE id = ?', (taskId,));
+        db.commit();
+
+    @staticmethod
+    def get_task(taskId: int) -> Task:
+        request = db.execute('SELECT * FROM tasks WHERE id = ?', (taskId,));
+        task = request.fetchone();
+        return Task(task[0], task[1], task[2], task[3], task[4]);
+
+    @staticmethod
+    def set_state(taskId : int):
+        db.execute('UPDATE tasks SET is_done = CASE WHEN is_done = 0 THEN 1 ELSE 0 END WHERE id = ?', (taskId,));
+        db.commit();
+
+    @staticmethod
+    def update_desc(taskId: int, new_desc: str):
+        db.execute('UPDATE tasks SET description = ? WHERE id = ?', (new_desc, taskId));
+        db.commit();
+
+    @staticmethod
+    def task_exists(taskId: int):
+        count = db.execute('SELECT COUNT(*) FROM tasks WHERE id = ?', (taskId,)).fetchone()
+        if count[0] > 0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def is_permitted(id_user: int, taskId: int):
+        task_owner_id = db.execute('SELECT id_user FROM tasks WHERE id = ?', (taskId,)).fetchone()
+        if task_owner_id[0] == id_user:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def filter(state: str, title: str, id_user: int):
+        is_done_l = 0
+        is_done_r = 1
+
+        if state == 'done':
+            is_done_l = 1
+        if state == 'in_progress':
+            is_done_r = 0
+
+        request = db.execute('SELECT * FROM tasks WHERE (is_done = ? OR is_done = ?) AND id_user = ? AND title LIKE ? ORDER BY id ASC', (is_done_l, is_done_r, id_user, '%' + title + '%'));
+        tasks = request.fetchall();
+        tasksList = [];
+        for task in tasks:
+            tasksList.append({
+                'id': task[0],
+                'title': task[1],
+                'description': task[2],
+                'user_id': task[3],
+                'state': task[4]
+            });
+        return tasksList;
